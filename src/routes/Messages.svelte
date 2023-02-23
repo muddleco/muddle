27c4a95751fd2c7d06d1feaf355a348d1a89efe4
Bailey Pumfleet <bailey@pumfleet.co.uk>
2023-02-23 19:06:09 +0000

    Add star and archive to Messages component

--- src/routes/Messages.svelte
+++ src/routes/Messages.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { slide } from 'svelte/transition';
-	import {decode} from 'html-entities';
+	import { decode } from 'html-entities';
+	import Icon from '@iconify/svelte';
 	import dayjs from 'dayjs';
 	import relativeTime from 'dayjs/plugin/relativeTime';
 	dayjs.extend(relativeTime);
@@ -9,6 +10,19 @@
 	export let hoverMessage;
 	export let showMessage;
 	export let selectedMessage;
+
+	const archiveMessage = async (e) => {
+		e.stopPropagation();
+		const res = await fetch('http://localhost:5173/api/archive?message=' + hoverMessage.id, {
+			method: 'POST'
+		});
+	};
+
+	const starMessage = async () => {
+		const res = await fetch('http://localhost:5173/api/star?message=' + hoverMessage.id, {
+			method: 'POST'
+		});
+	};
 </script>
 
 <div transition:slide>
@@ -26,9 +40,9 @@
 			>
 				{#if message.from.includes('<')}
 					{#if message.labels.includes('UNREAD')}
-						<div class="w-1.5 h-1.5 mt-1 -ml-3 mr-2 rounded-full bg-pink-500"></div>
+						<div class="w-1.5 h-1.5 mt-1 -ml-3 mr-2 rounded-full bg-pink-500" />
 					{/if}
-					<span class="font-medium w-2/12">{message.from.split('<')[0].replaceAll("\"", "")}</span>
+					<span class="font-medium w-2/12">{message.from.split('<')[0].replaceAll('"', '')}</span>
 				{:else}
 					<span class="font-medium w-2/12">{message.from.split('@')[0]}</span>
 				{/if}
@@ -37,7 +51,14 @@
 					<span class="ml-2 text-gray-500">{decode(message.snippet)}</span>
 				</div>
 				<div class="w-1/12 text-right text-gray-300">
-					<div class="hidden group-hover:block">Actions</div>
+					<div class="hidden group-hover:flex group-hover:space-x-1">
+						<div on:click|stopPropagation={starMessage} class="ml-auto">
+							<Icon icon="tabler:star" class="h-4 w-4 text-gray-400 hover:cursor-pointer hover:text-gray-600" />
+						</div>
+						<div on:click|stopPropagation={archiveMessage}>
+							<Icon on:click={archiveMessage} icon="tabler:check" class="h-4 w-4 text-gray-400 hover:cursor-pointer hover:text-gray-600" />
+						</div>
+					</div>
 					<span class="group-hover:hidden">{dayjs(message.date).fromNow()}</span>
 				</div>
 			</div>
