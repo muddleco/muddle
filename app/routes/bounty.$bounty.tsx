1834023b544d3385250bdf6c36f0971236aff205
Bailey Pumfleet <bailey@pumfleet.co.uk>
2023-12-11 17:59:00 -0500

    Show warning if submitted

--- app/routes/bounty.$bounty.tsx
+++ app/routes/bounty.$bounty.tsx
@@ -18,21 +18,47 @@ export default function Bounty() {
     <Shell heading={data.bounty?.name} user={data.user}>
       <div className="grid grid-cols-3 gap-x-4">
         <div className="col-span-2 border border-gray-100 rounded-lg px-7 py-4 text-sm">
+          {data?.bounty?.submissions &&
+            data?.bounty?.submissions?.length > 0 && (
+              <div className="border-b border-gray-100 pb-4 mb-4">
+                <h2 className="font-heading text-xl mb-4 text-orange-500">
+                  Pending review
+                </h2>
+                <p className="text-gray-500 mb-4">
+                  Someone has already made a submission for this bounty and
+                  it&apos;s currently pending review. You can check out their PR
+                  below, and decide if you want to submit your own solution.
+                </p>
+                <ul className="space-y-4">
+                  {data.bounty?.submissions.map((submission) => (
+                    <li key={submission.id}>
+                      <a
+                        href={submission.url}
+                        className="font-medium text-gray-900 hover:text-gray-700"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {submission.url}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
           <h2 className="font-heading text-xl mb-4">Description</h2>
-          {data?.bounty?.description}
+          <p className="text-gray-500">{data?.bounty?.description}</p>
         </div>
         <div className="space-y-4">
           <div className="bg-gray-50 border border-gray-100 rounded-lg px-7 py-4 text-sm">
             <h2 className="font-heading text-xl mb-4">Reward</h2>
             <p className="text-gray-900 mb-4">
-              An approved submission for this bounty will immediately receive ${data.bounty?.value}.
+              An approved submission for this bounty will immediately receive $
+              {data.bounty?.value}.
             </p>
           </div>
           <div className="bg-gray-50 border border-gray-100 rounded-lg px-7 py-4 text-sm">
             <h2 className="font-heading text-xl mb-4">GitHub</h2>
-            <p className="text-gray-900 mb-4">
-              View on GitHub &gt;
-            </p>
+            <p className="text-gray-900 mb-4">View on GitHub &gt;</p>
           </div>
           <div className="bg-gray-50 border border-gray-100 rounded-lg px-7 py-4 text-sm">
             <h2 className="font-heading text-xl mb-4">Submit</h2>
@@ -73,6 +99,7 @@ export async function loader({ params, request }) {
       id: params.bounty,
     },
     include: {
+      submissions: true,
       project: {
         include: {
           company: true,
